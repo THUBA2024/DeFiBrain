62c8bc6b49d0e8011d02424a8f68aa336124bb2c
zyy <84920215@qq.com>
2024-03-29 15:31:06 +0800

    add graph

--- graph_request.py
+++ graph_request.py
@@ -1,6 +1,13 @@
 import requests
 
 
+def Grequest(query):
+    key = "2fd57c65d0e051dea3ac417bdf3f6ad2"
+    url = "https://gateway-arbitrum.network.thegraph.com/api/2fd57c65d0e051dea3ac417bdf3f6ad2/subgraphs/id/JCNWRypm7FYwV8fx5HhzZPSFaMxgkPuw4TnR3Gpi81zk"
+    response = requests.post(url, json={'query': query})
+    data = response.json()
+    return data
+
 
 query = """
 {
@@ -16,8 +23,5 @@ query = """
 }
     """
 
-key = "2fd57c65d0e051dea3ac417bdf3f6ad2"
-url = "https://gateway-arbitrum.network.thegraph.com/api/2fd57c65d0e051dea3ac417bdf3f6ad2/subgraphs/id/JCNWRypm7FYwV8fx5HhzZPSFaMxgkPuw4TnR3Gpi81zk"
-response = requests.post(url, json={'query': query})
-data = response.json()
+data = Grequest(query)
 print(data)
